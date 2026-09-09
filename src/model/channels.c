ac2c82d20e23ee4518c6374faa2092babe7aaa7b
Dhruv Anand <ananddhruv29@gmail.com>
2026-09-10 00:28:01 +0530

    model: add per-cylinder columns to channel

--- src/model/channels.c
+++ src/model/channels.c
@@ -24,6 +24,8 @@ static double ch_oil_c(const ModelState *s, int i) {
   (void)i;
   return s->thermal.oil_temp_c;
 }
+static double ch_cyl_cht(const ModelState *s, int i) { return s->cyl[i].cht_c; }
+static double ch_cyl_egt(const ModelState *s, int i) { return s->cyl[i].egt_c; }
 
 static const ModelChannel CHANNELS[] = {
     {"rpm", "rpm", 2, -1, ch_rpm},
@@ -32,6 +34,14 @@ static const ModelChannel CHANNELS[] = {
     {"cht_c", "degC", 3, -1, ch_cht_c},
     {"egt_c", "degC", 3, -1, ch_egt_c},
     {"oil_c", "degC", 3, -1, ch_oil_c},
+    {"cht_c_1", "degC", 3, 0, ch_cyl_cht},
+    {"cht_c_2", "degC", 3, 1, ch_cyl_cht},
+    {"cht_c_3", "degC", 3, 2, ch_cyl_cht},
+    {"cht_c_4", "degC", 3, 3, ch_cyl_cht},
+    {"egt_c_1", "degC", 3, 0, ch_cyl_egt},
+    {"egt_c_2", "degC", 3, 1, ch_cyl_egt},
+    {"egt_c_3", "degC", 3, 2, ch_cyl_egt},
+    {"egt_c_4", "degC", 3, 3, ch_cyl_egt},
 };
 
 const ModelChannel *model_channels(int *count) {
