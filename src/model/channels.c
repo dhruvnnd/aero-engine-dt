58f03b37ee2f509abaf8b96a581a9c1c04dc9172
Dhruv Anand <ananddhruv29@gmail.com>
2026-09-10 04:46:17 +0530

    model: include fuel related column channels

--- src/model/channels.c
+++ src/model/channels.c
@@ -26,6 +26,21 @@ static double ch_oil_c(const ModelState *s, int i) {
 }
 static double ch_cyl_cht(const ModelState *s, int i) { return s->cyl[i].cht_c; }
 static double ch_cyl_egt(const ModelState *s, int i) { return s->cyl[i].egt_c; }
+static double ch_cyl_lambda(const ModelState *s, int i) {
+  return s->cyl[i].lambda;
+}
+static double ch_air_gps(const ModelState *s, int i) {
+  (void)i;
+  return s->fuel.air_flow_gps;
+}
+static double ch_fuel_kgph(const ModelState *s, int i) {
+  (void)i;
+  return s->fuel.fuel_flow_kgph;
+}
+static double ch_fuel_press(const ModelState *s, int i) {
+  (void)i;
+  return s->fuel.fuel_press_kpa;
+}
 
 static const ModelChannel CHANNELS[] = {
     {"rpm", "rpm", 2, -1, ch_rpm},
@@ -42,6 +57,13 @@ static const ModelChannel CHANNELS[] = {
     {"egt_c_2", "degC", 3, 1, ch_cyl_egt},
     {"egt_c_3", "degC", 3, 2, ch_cyl_egt},
     {"egt_c_4", "degC", 3, 3, ch_cyl_egt},
+    {"air_gps", "g/s", 3, -1, ch_air_gps},
+    {"fuel_kgph", "kg/h", 4, -1, ch_fuel_kgph},
+    {"fuel_press_kpa", "kPa", 2, -1, ch_fuel_press},
+    {"lambda_1", "-", 4, 0, ch_cyl_lambda},
+    {"lambda_2", "-", 4, 1, ch_cyl_lambda},
+    {"lambda_3", "-", 4, 2, ch_cyl_lambda},
+    {"lambda_4", "-", 4, 3, ch_cyl_lambda},
 };
 
 const ModelChannel *model_channels(int *count) {
